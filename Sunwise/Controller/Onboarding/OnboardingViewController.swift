15dc3c6ab884bfb842a30c01f65020e9d6dcd47f
urayfajri <uraywidiansyah88@gmail.com>
2022-12-20 13:08:01 +0700

    feat(Onboarding): create storyboard onboarding page

---
+++ Sunwise/Controller/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DashboardViewController.swift
+//  Sunwise
+//
+//  Created by Uray Muhamad Noor Fajri Widiansyah on 20/12/22.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    
+    @IBOutlet weak var setSkinProfileButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func setSkinProfileButtonTapped(_ sender: Any) {
+    }
+    
+
+}

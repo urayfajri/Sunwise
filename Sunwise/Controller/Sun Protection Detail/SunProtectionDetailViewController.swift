59abee91a7ef14ea972861253aecaed3d38b660a
Ariel Waraney Manueke <arielwaraneymanueke01@gmail.com>
2022-12-21 22:22:03 +0700

    feat(sun protection): add layout with constraint to the modal view in storyboard + connect outlet to the view controller

--- Sunwise/Controller/Sun Protection Detail/SunProtectionDetailViewController.swift
+++ Sunwise/Controller/Sun Protection Detail/SunProtectionDetailViewController.swift
@@ -9,6 +9,34 @@ import UIKit
 
 class SunProtectionDetailViewController: ViewController {
 
+    @IBOutlet weak var closeModalButton: UIButton!
+    @IBOutlet weak var protectionTitle: UILabel!
+    @IBOutlet weak var indexUV: UILabel!
+    
+    @IBOutlet weak var iconSunglasses: UILabel!
+    @IBOutlet weak var titleSunglasses: UILabel!
+    @IBOutlet weak var descSunglasess: UILabel!
+
+    @IBOutlet weak var iconClothes: UILabel!
+    @IBOutlet weak var titleClothes: UILabel!
+    @IBOutlet weak var descClothes: UILabel!
+    
+    @IBOutlet weak var iconSunscreen: UILabel!
+    @IBOutlet weak var titleSunscreen: UILabel!
+    @IBOutlet weak var descSunscreen: UILabel!
+    
+    @IBOutlet weak var iconHat: UILabel!
+    @IBOutlet weak var titleHat: UILabel!
+    @IBOutlet weak var descHat: UILabel!
+    
+    @IBOutlet weak var iconShelter: UILabel!
+    @IBOutlet weak var titleShelter: UILabel!
+    @IBOutlet weak var descShelter: UILabel!
+    
+    @IBOutlet weak var iconIndoor: UILabel!
+    @IBOutlet weak var titleIndoor: UILabel!
+    @IBOutlet weak var descIndoor: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 

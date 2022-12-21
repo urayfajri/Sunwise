904aaee5785e5f108bef01b3bac9aa626b1dba5d
Ariel Waraney Manueke <arielwaraneymanueke01@gmail.com>
2022-12-21 16:38:06 +0700

    feat(weather): add button see more function for modal view and modal view sun protection added to be appear only

---
+++ Sunwise/Controller/Sun Protection Detail/SunProtectionDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SunProtectionDetailViewController.swift
+//  Sunwise
+//
+//  Created by Ariel Waraney on 21/12/22.
+//
+
+import UIKit
+
+class SunProtectionDetailViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

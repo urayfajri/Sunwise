2bb32915b0901dab1c85e8348eabd2067375ff67
Ariel Waraney Manueke <arielwaraneymanueke01@gmail.com>
2022-12-30 14:09:48 +0800

    feat(UVCurrent): Remake UVCurrent Component view and connect with outlet

--- Sunwise/Component/UVCurrentView/UVCurrent.swift
+++ Sunwise/Component/UVCurrentView/UVCurrent.swift
@@ -9,34 +9,23 @@ import UIKit
 
 class UVCurrent: UIView {
 
-    @IBOutlet weak var uvi: UILabel!
-    @IBOutlet weak var categoryText: UILabel!
-    @IBOutlet weak var recommendationText: UILabel!
-    
+    @IBOutlet var uvi: UILabel!
+    @IBOutlet var categoryText: UILabel!
+    @IBOutlet var recommendationText: UILabel!
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+        viewInit()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        viewInit()
     }
     
-    func viewInit(){
+    func viewInit() {
         let xibView = Bundle.main.loadNibNamed("UVCurrent", owner: self)![0] as! UIView
         xibView.frame = self.bounds
         addSubview(xibView)
     }
-    
-    func setUviText(text: String){
-        self.uvi.text = text
-    }
-    
-    func setCategoryText(text: String){
-        self.categoryText.text = text
-    }
-
-    func setRecommendationText(text: String){
-        self.recommendationText.text = text
-    }
-    
 }
